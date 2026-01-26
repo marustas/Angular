e6e332ffe9ec4aec2210ddb3210362c735e01d7f
stastone <StanislauMarudau@coherentsolutions.com>
2026-01-26 16:08:11 +0200

    feat: some type shenanigans

--- src/app/app.ts
+++ src/app/app.ts
@@ -22,4 +22,31 @@ export class App {
     const input = event.target as HTMLInputElement;
     this.jokeService.setSearchTerm(input.value);
   }
+
+  ngOnInit() {
+    console.log('5' - -'2'); // 7
+    console.log('5' + -'2'); // '5-2'
+    console.log(true + false); // 1
+    console.log(true - false); // 1
+    console.log(null + 1); // 1
+    console.log(null == 0); // false
+    console.log(null >= 0); // true
+    console.log(undefined + 1); // NaN
+    console.log(undefined == null); // true
+    console.log(NaN === NaN); // false
+    console.log(0 == false); // true
+    console.log('' == false); // true
+    console.log([] == false); // true
+    console.log([] == ![]); // true because first [] is converted to false, ![] is converted to boolean, since all arrays and objects are truthy, ![] becomes false, so the comparison is false == false
+    console.log([0] == 0); // true
+    console.log([1, 2] == '1,2'); // true
+    console.log({} == '[object Object]'); // true
+    console.log({} + []); // '[object Object]'
+    console.log([] + []); // ''
+    console.log([] + {}); // '[object Object]'
+    console.log({} + {}); // '[object Object][object Object]'
+    console.log({} + []); // '[object Object]'
+    console.log([][[]] + []); // undefined, but STRING
+    console.log([][[]]); // undefined
+  }
 }
