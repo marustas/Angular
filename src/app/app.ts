f67277f96f20e3ed4c45e5f0ed0362b8adfe4acf
marustas <marudov.stas@gmail.com>
2026-01-28 10:03:30 +0200

    feat: more shenanigans with V8 event loop

--- src/app/app.ts
+++ src/app/app.ts
@@ -37,7 +37,7 @@ export class App {
     console.log(0 == false); // true
     console.log('' == false); // true
     console.log([] == false); // true
-    console.log([] == ![]); // true because first [] is converted to false, ![] is converted to boolean, since all arrays and objects are truthy, ![] becomes false, so the comparison is false == false
+    console.log([] == ![]); // [] == false -> [] == 0 -> '' == 0 -> 0 == 0 -> true
     console.log([0] == 0); // true
     console.log([1, 2] == '1,2'); // true
     console.log({} == '[object Object]'); // true
@@ -50,3 +50,111 @@ export class App {
     console.log([][[]]); // undefined
   }
 }
+
+//------------------------------------------------------------------
+//1
+
+(function () {
+  console.log(1);
+  setTimeout(function () {
+    console.log(2);
+  }, 1000);
+  setTimeout(function () {
+    console.log(3);
+  }, 0);
+  console.log(4);
+})();
+
+//------------------------------------------------------------------
+
+//2
+
+var myObject = {
+  foo: 'bar',
+  func: function () {
+    var self = this;
+    console.log(this.foo);
+    console.log(self.foo);
+    (function () {
+      console.log(this.foo);
+      console.log(self.foo);
+    })();
+  },
+};
+myObject.func();
+
+//---------------------------------------------------------
+//3
+
+for (let i = 0; i < 5; i++) {
+  setTimeout(function () {
+    console.log(i);
+  }, i * 1000);
+}
+//-----------------------------------------------------------
+//4
+
+function doSomething() {
+  x = 33;
+  console.log(x);
+  var x;
+}
+doSomething();
+
+//---------------------------------------------------------
+//5
+
+var x = 2;
+var y = '2';
+console.log(x == y);
+
+console.log(x === y);
+
+//---------------------------------------------------------
+//6
+
+var obj = { name: 'Name', surname: 'Surname' };
+
+var obj2 = obj;
+obj.name = 'new value';
+console.log(obj2.name);
+obj = null;
+console.log(obj2.name);
+
+//---------------------------------------------------------
+//7
+console.log('1');
+
+setTimeout(() => console.log('2'), 0);
+
+Promise.resolve().then(() => {
+  console.log('3');
+
+  queueMicrotask(() => console.log('4'));
+
+  Promise.resolve().then(() => console.log('5'));
+
+  setTimeout(() => console.log('6'), 0);
+});
+
+(async () => {
+  console.log('7');
+
+  await null;
+
+  console.log('8');
+
+  await Promise.resolve().then(() => console.log('9'));
+
+  console.log('10');
+})();
+
+queueMicrotask(() => {
+  console.log('11');
+
+  Promise.resolve().then(() => console.log('12'));
+});
+
+console.log('13');
+
+// Output 1 -> 7 -> 13 -> 3 -> 8 -> 11 -> 4 -> 5 -> 9 -> 12 -> 10 -> 2 ->6
